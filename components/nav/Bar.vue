df79f5e77265427b89c9ba122c674cebe3e2b7c9
黄新民 <huangxinmin@hzwanglan.com>
2023-06-25 15:43:01 +0800

    fix:修改

--- components/nav/Bar.vue
+++ components/nav/Bar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="border-b border-slate-200 px-5 py-2 flex items-center justify-between">
-    <h1 class="text-2xl font-bold">Nuxt3 in Action2</h1>
+    <h1 class="text-2xl font-bold">Nuxt3 in Action3232</h1>
     <label>
       dark mode:
       <NSwitch id="toggle" v-model:value="appConfig.theme.dark" />
